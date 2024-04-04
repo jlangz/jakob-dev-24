dc5d2bba1daa8a54a2800b03f1e94a94b0510cb7
JLangz <jakob.langseth@gmail.com>
2024-04-04 22:48:56 +0200

    Moving overflow to wrapper

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -101,7 +101,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       <div className="grid min-h=[70vh] grid-cols-1 md:grid-cols-2 items-center">
         <Shapes>
         </Shapes>
-        <div className="col-start-1 md:row-start-1">
+        <div className="col-start-1 overflow-hidden md:row-start-1">
           <h1 
             className="mb-8 text-[clamp(3rem,20vmin,9rem)] font-extrabold leading-none trackin-tighter" 
             aria-label={slice.primary.first_name + " " + slice.primary.last_name}
@@ -114,7 +114,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             </span>
           </h1>
           <span onClick={handleTagLineClick}
-            className="sub-text cursor-pointer block bg-gradient-to-tr from-purple-500 via-sky-300 to-purple-500 bg-clip-text overflow-hidden text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl"
+            className="sub-text cursor-pointer block bg-gradient-to-tr from-purple-500 via-sky-300 to-purple-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl"
           >
               {selectedTagLine}
           </span>
